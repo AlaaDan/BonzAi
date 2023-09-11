addfafc036f773ea2888a557764e1dc86878d7da
Alaa Aldandachi <audio-net@hotmail.com>
2023-09-11 20:00:42 +0200

    bug fix in the serverless.yml file

--- functions/postBooking/index.js
+++ functions/postBooking/index.js
@@ -1,9 +1,32 @@
+const { nanoid } = require('nanoid')
 const { sendResponse, sendError } = require('../../responses/index')
 const { db } = require('../../services/db')
 
 exports.handler = async (event, context) => {
+    const { name, person, date, time } = JSON.parse(event.body)
 
-    return sendResponse(200, {
-        message: 'Hello from PostBooking Lambda'
-    })
+    try {
+        const id = nanoid()
+        const result = await db.collection('bookings').add({
+            name,
+            person,
+            date,
+            time,
+            id
+        })
+
+        return sendResponse(200, {
+            message: 'Booking successfully created',
+            id: result.id,
+            name: result.name,
+            person: result.person,
+            date: result.date,
+            time: result.time
+
+        })
+    } catch (error) {
+        console.log(error)
+        return sendError(500, error)
+    }
+    
 }
