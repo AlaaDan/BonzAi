b855138f8a729324a3b94006b4f6435c3d753d89
Alaa Aldandachi <audio-net@hotmail.com>
2023-09-12 18:37:40 +0200

    Post a booking function has been updated as well as some bug fixes

--- functions/postBooking/index.js
+++ functions/postBooking/index.js
@@ -3,30 +3,74 @@ const { sendResponse, sendError } = require('../../responses/index')
 const { db } = require('../../services/db')
 
 exports.handler = async (event, context) => {
-    const { name, person, date, time } = JSON.parse(event.body)
+    const { name, person, date } = JSON.parse(event.body)
 
     try {
-        const id = nanoid()
-        const result = await db.collection('bookings').add({
+        
+        const item = {
+            bookingId: nanoid(),
             name,
             person,
             date,
-            time,
-            id
-        })
+        }
+        await db.put({
+            TableName: 'Bonz-db',
+            Item: {...item},
+        }).promise()
 
         return sendResponse(200, {
             message: 'Booking successfully created',
-            id: result.id,
-            name: result.name,
-            person: result.person,
-            date: result.date,
-            time: result.time
+            booking: {...item},
 
         })
+
     } catch (error) {
         console.log(error)
         return sendError(500, error)
     }
     
 }
+//const { v4: uuidv4 } = require('uuid'); // Import the uuid package
+
+// const { sendResponse, sendError } = require('../../responses/index');
+// const { db } = require('../../services/db');
+
+// exports.handler = async (event, context) => {
+//   try {
+   
+//     const requestBody = JSON.parse(event.body);
+
+//     // Generate a random ID for the booking
+//     const bookingId = nanoid();
+
+//     // TODO: Validate the booking details (e.g., check for required fields)
+
+//     // Create a new booking object
+//     const booking = {
+//       bookingId, 
+//       guestName: requestBody.guestName,
+//       checkInDate: requestBody.checkInDate,
+//       checkOutDate: requestBody.checkOutDate,
+//       roomType: requestBody.roomType,
+      
+//     };
+
+//     // Store the booking in DynamoDB
+//     const params = {
+//       TableName: 'bonz-db', 
+//       Item: booking,
+//     };
+
+//     await db.put(params).promise();
+
+//     return sendResponse(200, {
+//       message: 'Booking successful',
+//       bookingId, 
+//     });
+//   } catch (error) {
+//     console.error('Error creating booking:', error);
+//     return sendError(500, {
+//       message: 'Internal server error',
+//     });
+//   }
+// };
